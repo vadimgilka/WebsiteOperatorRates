117bd92f493f12735d76ac3852d5e923cb0b10c4
asdadsdasd <mkozarez12345@mail.ru>
2024-05-05 23:37:02 +0300

    init project

---
+++ Project/WebsiteOperatorRates/src/main/java/ru/kozarez/websiteoperatorrates/WebsiteOperatorRatesApplication.java
@@ -0,0 +1,13 @@
+package ru.kozarez.websiteoperatorrates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteOperatorRatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteOperatorRatesApplication.class, args);
+	}
+
+}
---
+++ Project/WebsiteOperatorRates/src/test/java/ru/kozarez/websiteoperatorrates/WebsiteOperatorRatesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kozarez.websiteoperatorrates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteOperatorRatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
